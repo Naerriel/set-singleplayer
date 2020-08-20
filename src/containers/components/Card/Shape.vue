53dca92f8f8aa7e55fe5fe7297f8bfdf5baada75
Naerriel <naerriel@gmail.com>
2020-08-20 15:56:59 +0200

    Simplify Shape.vue

--- src/containers/components/Card/Shape.vue
+++ src/containers/components/Card/Shape.vue
@@ -4,25 +4,15 @@
       :color="color"
     />
     <path
-      v-if="shape === 'oval'"
       :class="pathClass"
-      :d="ovalD"
-    />
-    <path
-      v-if="shape === 'squiggle'"
-      :class="pathClass"
-      :d="squiggleD"
-    />
-    <path
-      v-if="shape === 'diamond'"
-      :class="pathClass"
-      d="M38.8,11.61l25,50-25,50-25-50Z"
+      :d="pathD"
     />
   </svg>
 </template>
 
 <script>
   import Stripes from './Stripes.vue';
+  import { CardShape } from '../../../logic/model';
 
   export default {
     props: {
@@ -40,22 +30,24 @@
       }
     },
 
-    data() {
-      return {
-        ovalD: `M39.26,114.16a19.46,19.46,0,0,1-19.5-19.5v-60a19.5,19.5,0,0,1,39,
-          0v60A19.46,19.46,0,0,1,39.26,114.16Z`,
-        squiggleD: `M52.45,74.56c0,16.1,11,19.9,10.6,28.3-.5,9.2-21.1,12.2-33.4,3.8s-15.8-21.2-9.3-38a42.48,
-          42.48,0,0,0,4.8-20c0-16.1-11-19.9-10.6-28.3.5-9.1,21.1-12.2,33.4-3.7s15.8,21.2,9.3,38C54.45,61.76,
-          52.55,68.56,52.45,74.56Z`,
-      };
-    },
-
     computed: {
       pathClass() {
         if (this.shading === 'striped') {
           return `${this.shading}-${this.color}`;
         }
         return `${this.shading} ${this.color}`;
+      },
+
+      pathD() {
+        const paths = {
+          [CardShape.diamond]: 'M38.8,11.61l25,50-25,50-25-50Z',
+          [CardShape.oval]: `M39.26,114.16a19.46,19.46,0,0,1-19.5-19.5v-60a19.5,19.5,0,0,1,39,
+          0v60A19.46,19.46,0,0,1,39.26,114.16Z`,
+          [CardShape.squiggle]: `M52.45,74.56c0,16.1,11,19.9,10.6,28.3-.5,9.2-21.1,12.2-33.4,3.8s-15.8-21.2-9.3-38a42.48,
+            42.48,0,0,0,4.8-20c0-16.1-11-19.9-10.6-28.3.5-9.1,21.1-12.2,33.4-3.7s15.8,21.2,9.3,38C54.45,61.76,
+            52.55,68.56,52.45,74.56Z`,
+        };
+        return paths[this.shape];
       },
     },
 
